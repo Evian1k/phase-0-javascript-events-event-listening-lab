a41351f4648a7db813ce539bb972ba7b077c5a86
Evian1k <emmanuelevian@gmail.com>
2025-03-25 23:50:18 +0300

    Adds update

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const button = document.getElementById('button');
+    console.log(button); // Debugging step
+    if (button) {
+        button.addEventListener('click', function() {
+            alert('Button clicked!');
+        });
+    } else {
+        console.error("Button not found!");
+    }
+}
